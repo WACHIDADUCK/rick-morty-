1cee38b11c12f669703fc2b45f50e743d4458f8c
WACHIDADUCK <captainvantruc@gmail.com>
2025-02-04 16:52:19 +0000

    commit

--- src/pages/BlogCharacter.jsx
+++ src/pages/BlogCharacter.jsx
@@ -1,15 +1,33 @@
 import { StorageContext } from "../App"
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 export default function BlogCharacter() {    
 
     const {storage, setStorage} = useContext(StorageContext);
-    
-    console.log(storage)
+    const [busquedaPersonaje, setBusquedaPersonaje] = useState('');
+    const [mostrarPersonajes, setMostrarPersonajes] = useState([]);
 
+
+    useEffect(() => {
+        setMostrarPersonajes(storage.results)
+    },[])
+
+
+    useEffect(() => {
+
+        setMostrarPersonajes(storage.results.filter((character) => character.name.toLowerCase().includes(busquedaPersonaje.toLowerCase())));
+
+    },[busquedaPersonaje])
+
+    
     return (    
-        <div>    
-            Blog
+        <div className="containerPersonajes">
+            <input type="text" value={busquedaPersonaje} onChange={(e) => setBusquedaPersonaje(e.target.value)} placeholder="Busqueda tu personaje" />
+            {
+                mostrarPersonajes.map((character) => {return <Link key={character.id} to={`/personaje/${character.id}`} className="">{character.name}</Link>})
+            }
+
         </div>    
     )    
 }
--- src/pages/Personaje.jsx
+++ src/pages/Personaje.jsx
@@ -6,14 +6,16 @@ import { StorageContext } from "../App"
 export default function Personaje() {
 
     const params = useParams();
-    const {storage} = useContext(StorageContext);
+    const {storage} = useContext(StorageContext); 
+
+    const personaje = storage.results.find(personaje => personaje.id == params.id);
 
     return (
       <div className="card">
-            <img src={storage.results[params.id].image} className="card-img-top" alt="..."></img>
+            <img src={personaje.image} className="card-img-top" alt="..."></img>
             <div className="card-body">
-                <h5 className="card-title">{storage.results[params.id].name}</h5>
-                <p className="card-text">{storage.results[params.id].species}</p>
+                <h5 className="card-title">{personaje.name}</h5>
+                <p className="card-text">{personaje.species}</p>
                 <Link to={`/`} className="btn btn-primary">Volver</Link>
             </div>
       </div>
